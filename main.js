d7d8fc2c5e3ed940475be5f194d513b82617606c
Yang Lin <97666098+ylf13@users.noreply.github.com>
2022-05-03 22:15:12 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+// this is my age
+var myAge = 24;
+
+// earlyYears
+var earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+// laterYears
+laterYears = myAge - 2;
+
+// number of dog years accounted for by laterYears
+laterYears *= 4;
+
+//  my age in dog years
+var myAgeInDogYears = earlyYears + laterYears;
+
+//  converting myName to lower case
+var myName = 'Fang Yang Lin';
+myName.toLowerCase();
+
+console.log('My name is ${myName}.I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.');
